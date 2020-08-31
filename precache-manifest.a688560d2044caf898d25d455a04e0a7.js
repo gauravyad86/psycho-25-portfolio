428ecc539949b82492577195c27b84dae2fba316
Gaurav Yadav <ygauravyy@gmail.com>
2020-08-31 09:10:19 +0530

    initial push
    Gaurav's Portfolio

---
+++ precache-manifest.a688560d2044caf898d25d455a04e0a7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109b8e9a764ce31b007e9aecb19831bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02dcd22c679e0fb42f5b",
+    "url": "/static/js/2.f2ff7091.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2ff7091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a6b0fa37e179dcdead",
+    "url": "/static/js/main.093736f3.chunk.js"
+  },
+  {
+    "revision": "ffc5236c1cd0353e3850",
+    "url": "/static/js/runtime-main.bd1c6f22.js"
+  },
+  {
+    "revision": "9c3c90dd1e9fcb5e018b11f96fea9bf8",
+    "url": "/static/media/add.9c3c90dd.png"
+  },
+  {
+    "revision": "7718bd2e1fe2a6fddbd2da459a563c75",
+    "url": "/static/media/car-customization.7718bd2e.jpg"
+  },
+  {
+    "revision": "82f9f9434501d00b380629a3823f0524",
+    "url": "/static/media/crafting.82f9f943.jpg"
+  },
+  {
+    "revision": "438848006c313c909c1ca052e6384b24",
+    "url": "/static/media/dashboard.43884800.png"
+  },
+  {
+    "revision": "9e82b711791bf3a185349b95626d09da",
+    "url": "/static/media/gameplay.9e82b711.jpg"
+  },
+  {
+    "revision": "38a897624897166cd1d970ce59b66334",
+    "url": "/static/media/games-grid.38a89762.jpg"
+  },
+  {
+    "revision": "eda16d6a7de4f464f2f2d75ea0494a37",
+    "url": "/static/media/helicopter.eda16d6a.jpg"
+  },
+  {
+    "revision": "12c01d5eff81e1779b984ffcb4e61d34",
+    "url": "/static/media/ietf.12c01d5e.jpg"
+  },
+  {
+    "revision": "3221f2449ef045838d0bdd64d2ce58a3",
+    "url": "/static/media/logo.3221f244.png"
+  },
+  {
+    "revision": "d2af7be3a66e7d42bb2710aa1ad9ac59",
+    "url": "/static/media/logo.d2af7be3.png"
+  },
+  {
+    "revision": "d615b6548ebb94b943939dd9f4ffb3d3",
+    "url": "/static/media/logo.d615b654.png"
+  },
+  {
+    "revision": "8d5bbc76d4ac1f3632415a3fca084bd3",
+    "url": "/static/media/main.8d5bbc76.png"
+  },
+  {
+    "revision": "a42d3e6af7edcf7c60a455c55bfc00c4",
+    "url": "/static/media/main.a42d3e6a.jpg"
+  },
+  {
+    "revision": "b52b097acca082df227dd54e881820c3",
+    "url": "/static/media/main.b52b097a.jpg"
+  },
+  {
+    "revision": "7916ca2087b954cca5531a07e1201157",
+    "url": "/static/media/passwords.7916ca20.png"
+  },
+  {
+    "revision": "4ac4ed086c2b871a3acf88735871322d",
+    "url": "/static/media/plane.4ac4ed08.jpg"
+  },
+  {
+    "revision": "7284c267960db508fa1d90056c396eea",
+    "url": "/static/media/pp.7284c267.jpeg"
+  },
+  {
+    "revision": "67012220d3829b00524ec3cc4c029ed3",
+    "url": "/static/media/profile.67012220.png"
+  },
+  {
+    "revision": "e8e1b5d506eea2d32a28b29d4801bdaf",
+    "url": "/static/media/prototypes.e8e1b5d5.jpg"
+  },
+  {
+    "revision": "99f2008c290f4cdebe0a75bddf821eb0",
+    "url": "/static/media/survival.99f2008c.jpg"
+  },
+  {
+    "revision": "da30a8e05a8ec8de5f8d566e37c6f4f0",
+    "url": "/static/media/terrains.da30a8e0.jpg"
+  },
+  {
+    "revision": "e78d3821c9af6ce6673cf58488f4c43c",
+    "url": "/static/media/upgrades.e78d3821.jpg"
+  }
+]);
